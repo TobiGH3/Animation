1857c6cdbb1461037228d2d1fff19c8b2e067efb
TobiGH3 <>
2017-06-27 16:55:00 +0200

    kek

--- app/src/main/java/com/tschwarze/animation/MainActivity.java
+++ app/src/main/java/com/tschwarze/animation/MainActivity.java
@@ -4,10 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
-
+//asdhjkasdkjh
 //https://github.com/daimajia/AndroidViewAnimations
 public class MainActivity extends AppCompatActivity {
 
